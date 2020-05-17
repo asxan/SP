30bb2b3d0997ea8e3a72af452b25188115921460
asxan <40429184+asxan@users.noreply.github.com>
2020-05-17 21:41:37 +0300

    Add five labwork

---
+++ Lab_5/Lab_5_1/Lab_5_1/Lab_5_1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+
+int main()
+{
+	HANDLE mutex = CreateMutex(NULL, FALSE, TEXT("UNIQUE_MUTEX"));
+	if (WaitForSingleObject(mutex, 0) == WAIT_OBJECT_0) {
+
+		_tprintf(TEXT("Running..."));
+	}
+	else {
+		_tprintf(TEXT("Program is already running. Fatal Error."));
+	}
+	getchar();
+	return 0;
+}
